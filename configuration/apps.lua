7abb6d0f2fb602a524b2c11448dbabe6417c5dd8
wolfiediscord <wolfiediscord@gmail.com>
2020-10-04 22:09:00 -0500

    Change terminal emulator to st

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,7 +8,7 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'terminator',
+    terminal = 'st',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
     quake = 'terminator',
